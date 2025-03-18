6a3269a14782bdb5fb34cbbdae5abbe171233620
Patric <pmorevs@gmail.com>
2025-03-18 13:43:55 +0200

    definit integral ready

--- script.js
+++ script.js
@@ -5,5 +5,22 @@ function myF(x){
 }
 
 var xStart = 0, xEnd = 5;
-var numberOfIntervals = 5;
+var numberOfIntervals = 100000000;
 var deltaX = (xEnd - xStart) / numberOfIntervals;
+
+console.log("xStart = " + xStart + "; xEnd = " + xEnd);    
+console.log("Number of intervals = " + numberOfIntervals);
+console.log("dX = " + deltaX);  
+
+var area = 0;
+
+for(let i = 0; i < numberOfIntervals; i++){
+    var x0 = xStart + i*deltaX;
+    var x1 = xStart + (i + 1)*deltaX;
+    var middlePoint = (x0 + x1) / 2;
+    var myFuncValue = myF(middlePoint);
+
+    area += myFuncValue*deltaX;    
+}
+
+console.log(`Area = ${area}`);
